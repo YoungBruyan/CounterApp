d49b33d4f0893f348e3b1b140db79d7a5727de61
Daniel Celis <bryan_dca@hotmail.com>
2022-03-24 02:31:13 -0600

    First Commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,31 @@
+
+
+import PropTypes from 'prop-types'
+
+
+
+const PrimeraApp = ({ saludo, subtitulo }) =>{
+
+    
+
+
+    
+
+    return (
+        <>
+            <h1>{ saludo }</h1>
+            
+            <p>{ subtitulo }</p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes={
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps= {
+    subtitulo: 'Soy un subtitulo xd'
+}
+
+export default PrimeraApp;
